9384a33d08900c5ecf91f2b3c6a6aadeef7ff5a4
Adam Johnson <me@adamj.eu>
2023-07-10 13:43:53 +0100

    Drop Python 3.7 support
    EOL on 2023-06-27: https://devguide.python.org/versions/ .

--- tests/test_flake8_no_pep420.py
+++ tests/test_flake8_no_pep420.py
@@ -1,15 +1,10 @@
 from __future__ import annotations
 
 import re
-import sys
+from importlib.metadata import version
 from textwrap import dedent
 
 import pytest
-
-if sys.version_info >= (3, 8):
-    from importlib.metadata import version
-else:
-    from importlib_metadata import version
 
 
 @pytest.fixture
